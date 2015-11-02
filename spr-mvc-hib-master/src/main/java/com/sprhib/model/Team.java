f8ccbdeea3671bc3c4c8dee517d7cf985d46e086
Guan <zguan@doe.state.nj.us>
2015-11-02 14:19:45 -0500

    first

---
+++ spr-mvc-hib-master/src/main/java/com/sprhib/model/Team.java
@@ -0,0 +1,46 @@
+package com.sprhib.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.context.annotation.Scope;
+
+@Entity
+@Table(name="teams")
+@Scope("prototype")
+public class Team {
+	
+	@Id
+	@GeneratedValue
+	@Column(name = "id")
+	private Integer id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "rating")
+	private Integer rating;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getRating() {
+		return rating;
+	}
+	public void setRating(Integer rating) {
+		this.rating = rating;
+	}
+
+}
